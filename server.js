0fdf38d9d65361bb132c3ee64a009e43311dce06
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-12-08 06:09:02 +0700

    Update server file

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+// const bodyParser = require('body-parser');
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const db = require('../database');
+
 const app = express();
+const PORT = process.env.PORT || 8080;
+console.log('PORT IS: ', PORT);
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -18,4 +29,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
